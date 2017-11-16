3992754fc7394be454ee1e61417ac2ac20036645
xiongyong <xiongyong@hxxn.onaliyun.com>
2017-11-16 17:19:41 +0800

    项目提交

--- EMS/app/src/main/java/com/hxxn/ems/view/CallBack_ScrollChanged.java
+++
@@ -1,6 +0,0 @@
-package com.hxxn.ems.view;
-
-
-public interface CallBack_ScrollChanged {
-    void onScrollChanged(int vertical);
-}
--- EMS/app/src/main/java/com/hxxn/ems/view/base/baseadapter/OnItemLongClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.hxxn.ems.view.base.baseadapter;
-
-/**
- * Created by jingbin on 16/7/4.
- */
-public interface OnItemLongClickListener<T> {
-    public void onLongClick(T t, int position);
-}
